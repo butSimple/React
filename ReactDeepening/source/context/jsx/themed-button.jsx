9697e2bf7a1f9691ee02f57c45b593f963189f99
ParkChunkyoung <parkhg092@naver.com>
2019-05-24 17:32:53 +0900

    webpack 모듈 커밋

---
+++ ReactDeepening/source/context/jsx/themed-button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ThemeContext} from './theme-context.jsx';
+
+class ThemedButton extends React.Component{
+  render(){
+    const props = this.props;
+    const theme = this.context;
+
+    return (
+      <button
+        {...props} style={{backgroundColor:theme.background}}
+      />
+    );
+  }
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
